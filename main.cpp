b14467c557741f25d43dadc5e947119020a7e5dd
fwsmit <fw.smit01@gmail.com>
2018-10-14 14:13:03 +0200

    can ask questions and check answers

---
+++ main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <fstream>
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <ctype.h>
+
+using namespace std;
+
+struct Word{
+	string question, answer;
+	Word(string _question, string _answer) : question(_question), answer(_answer){}
+};
+
+bool askQuestion(Word word);
+void removeUnneccesaryCharacters(string& str, bool isAnswer = false);
+template <typename I>
+size_t random_element(I begin, I end);
+
+int main(){
+	std::srand(std::time(0));
+	std::vector<Word> words;
+	ifstream words_file;
+	words_file.open("woordjes.txt") ;
+	if(words_file.is_open()){
+		string line;
+		while(getline(words_file, line)){
+			std::size_t equalSign = line.find('=');
+			if(equalSign != std::string::npos){
+				std::cout << "Found equal sign at: " << equalSign << endl;
+				string question = line.substr(0, equalSign);
+				removeUnneccesaryCharacters(question, false);
+				//std::cout << "Question is:" << question << ",understand?" << endl;
+				string answer = line.substr(equalSign); // take substring from equal sign
+				removeUnneccesaryCharacters(answer);
+				//std::cout << "Answer is:" << answer << ".OK?" << endl;
+				words.push_back(Word(question, answer));
+			}
+			else {
+				//std::cout << "didn't find equal sign" << endl;
+			}
+			
+		}
+	}
+	else {
+		cout << "unable to open file" << endl;
+		return -1;
+	}
+	std::cout << "Number of words to test: " << words.size() << endl;
+	std::vector<bool> correct(words.size(), false);
+	bool allCorrect = false;
+	while(!allCorrect){
+		size_t index;
+		do{
+			index = random_element(words.begin(), words.end());
+		}while(correct[index] == true);
+		
+		bool isCorrect = askQuestion(words[index]);
+		if(isCorrect){
+			correct[index] = true;
+		}
+		
+		allCorrect = true;
+		for(auto i : correct){
+			if (i == false) {
+				allCorrect = false;
+			}
+			
+		}
+	}
+}
+
+bool askQuestion(Word word){
+	bool correct = false;
+	std::cout << word.question << endl;
+	
+	string answer;
+	std::getline(std::cin, answer);
+	//std::cout << "rightAnswer" << word.answer << endl;
+	//std::cout << "answer" << answer << endl;
+	removeUnneccesaryCharacters(answer);
+	if (answer == word.answer) {
+		std::cout << "Correct" << endl;
+		correct = true;
+	}
+	else{
+		std::cout << "Wrong, it should be: " << word.answer << endl;
+	}
+	return correct;
+}
+
+void removeUnneccesaryCharacters(string& str, bool isAnswer){
+	if(str.size() > 0){
+		std::size_t equalSign = str.find('=');
+		if(equalSign != std::string::npos){
+			str.erase(equalSign, 1);
+		}
+		// remove whitespace from beginning
+		while(isspace(str[0])){
+			str.erase(0, 1);
+		}
+		//// remove whitespace from end
+		while(isspace(str[str.size() -1])){
+			str.erase(str.size()-1, 1);
+		}
+		if(isAnswer){
+			for(int i = 1; i < str.size(); i++){
+				if(isspace(str[i]) && std::ispunct(str[i-1])){
+					str.erase(i, 1);
+					i = 1;
+				}
+			}
+		}
+	}
+}
+
+template <typename I>
+size_t random_element(I begin, I end)
+{
+    const unsigned long n = std::distance(begin, end);
+    const unsigned long divisor = (RAND_MAX) / n;
+
+    unsigned long k;
+    do { k = std::rand() / divisor; } while (k >= n);
+
+    std::advance(begin, k);
+    return k;
+}
